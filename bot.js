e427abe2bf2da7b2eae78e7ce0767ba5bbeebdd6
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-01 04:44:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -143,6 +143,34 @@ client.on("guildMemberRemove", (member) => {
 client.channels.get('485278175740428289').edit({name : `أعضاء السيرفر : ${member.guild.memberCount} `});
 })
 
+client.on('message', message => {
+  if (message.author.codes) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  var command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+var args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** هذا الأمر لسيرفرات فقط **');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("** أنا لا أملك خاصيه  ` BAN_MEMBERS ` **");
+  let user = message.mentions.users.first();
+  
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
+
+
+  message.guild.member(user).ban(7, user);
+
+message.channel.send(`**✅ ${user.tag} banned from the server ! ✈ **  `)
+
+}
+}); 
+
 
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
