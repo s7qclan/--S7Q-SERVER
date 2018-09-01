f7bb346855b09cd8020113215dc5bddb4d0c3126
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-01 08:40:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -502,5 +502,43 @@ client.on('ready', () => {
 
 });
 
+let rebel;
+client.on("ready", async  => {
+    let guild = client.guilds.get("442919008715669505");
+  let users = guild.members.map(member => member.user.id);
+  let i;
+  rebel=0;
+for (i=0 ; i < users.length ; i++) {
+ let   check = guild.members.get(users[i]);
+if(!check.voiceChannelID){
+        continue;
+}else{
+  rebel++;
+}
+}
+guild.channels.find('id', '485337917146005505').setName("S7Q Voice Online ["+rebel+"]");
+  client.setInterval(() =>{
+    let d = Date.now()
+  }, 5000);
+});
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let guild = client.guilds.get("442919008715669505");
+let newUserChannel = newMember.voiceChannel
+let oldUserChannel = oldMember.voiceChannel
+ if(oldUserChannel === undefined && newUserChannel !== undefined) {
+   rebel++;
+guild.channels.find('id', '485337917146005505').setName(" S7Q Voice Online ["+rebel+"]");
+} else if(newUserChannel === undefined){
+  rebel--;
+guild.channels.find('id', '485337917146005505').setName(" S7Q Voice Online ["+rebel+"]");
+}
+});
+client.on('message', Codes => {
+  
+  if(Codes.content === "!صوت") {
+      Codes.channel.send(" S7Q Voice Online ["+rebel+"]");
+}
+});
+
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
