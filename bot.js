5f537ce10ccb94c3330f9282dd0246493faf2da5
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-01 05:18:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -421,43 +421,7 @@ client.on("message", message => {
 	} 
 });
 
-const bannedwords = [
-  "#credit",
-  "#profile",
-  "#rep",
-  "#top",
-  "رصيد",
-  "%تقديم",
-  "-play",
-  "-stop",
-  "-p",
-  "-s",
-  "!invites",
-  "!top",
-  "G.play",
-  "G.stop",
-  "G.skip",
-  "-skip"
 
-]
-client.on('message', message => {
-  var Muted = message.guild.roles.find("name", "muted");
-  var warn = message.guild.roles.find("name", "warn");
-  if(bannedwords.some(word => message.content.includes(word))) {
-  if(message.channel.id !== '476806402787246100') return;
-  if (message.author.bot) return;
-  if(message.member.roles.has(warn)) return;
-  if(!message.member.roles.has(warn.id)) {
-  message.member.addRole(warn)
-  message.reply("**`تم اعطائك تحذير لاستخدام اوامر البوت فى الشات العام` 😠**")
-  }
-  if(message.member.roles.has(warn.id)) {
-      message.member.addRole(Muted)
-      message.member.removeRole(warn)
-      message.reply("**`تم اعطائك ميوت كتابى تواصل مع احد اعضاء الادارة لازالتة` 🤐**")
-  }
-  }
-  })
 
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
