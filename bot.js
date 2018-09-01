6a48669c1fd097ed6f78ed9e497fd65c7a0fa109
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-01 05:01:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -311,17 +311,47 @@ message.react("❌")
     });
 
 client.on('message', message => {
-if(message.content === `${prefix}voiceonline`) {
-  message.guild.createChannel(`Voice Online : ${message.guild.members.filter(g => g.voiceChannel).size} ` , "voice").then(c => {
+if(message.content === `${prefix}VVV`) {
+  message.guild.createChannel(`S7Q Voice Online : ${message.guild.members.filter(g => g.voiceChannel).size} ` , "voice").then(c => {
    c.overwritePermissions(message.guild.id, {CONNECT: false});
-    message.channel.send(`**Voice Online : ${message.guild.members.filter(g => g.voiceChannel).size}**`);
+    message.channel.send(`**S7Q Voice Online : ${message.guild.members.filter(g => g.voiceChannel).size}**`);
     setInterval(() => {
-    c.setName(`Voice Online : ${message.guild.members.filter(g => g.voiceChannel).size} `)
+    c.setName(`S7Q Voice Online : ${message.guild.members.filter(g => g.voiceChannel).size} `)
     },1000);
   })
 
 }
 });
+
+client.on('message', message => { 
+	var prefix ="!";
+           if (message.content.startsWith(prefix + "id")) {
+     var args = message.content.split(" ").slice(1);
+     let user = message.mentions.users.first();
+     var men = message.mentions.users.first();
+        var heg;
+        if(men) {
+            heg = men
+        } else {
+            heg = message.author
+        }
+      var mentionned = message.mentions.members.first();
+         var h;
+        if(mentionned) {
+            h = mentionned
+        } else {
+            h = message.member
+        }
+               moment.locale('ar-TN');
+      var id = new  Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL) 
+    .setColor("#707070")
+    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
+    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
+    .setFooter(`sliver Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
+    .setThumbnail(heg.avatarURL);
+    message.channel.send(id)
+}       });
 
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
